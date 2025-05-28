a38682753d12decc2ae7caad105ff7225a6b0a8c
valarmathi <valarmathib001@gmail.com>
2025-05-28 19:45:43 +0530

    added the BACK-END setup

---
+++ Job-Portal-BE/src/main/java/com/example/Job_Portal_BE/JobPortalBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Job_Portal_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalBeApplication.class, args);
+	}
+
+}
---
+++ Job-Portal-BE/src/test/java/com/example/Job_Portal_BE/JobPortalBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Job_Portal_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
